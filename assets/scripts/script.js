a3d093454cee3df704c856595817b71135cf630c
Matt Randolph <mttrndlph@gmail.com>
2021-01-10 16:08:25 -0500

    Added the APIs I will use

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+
+    var weatherApi= api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=ab246c1d8eb84670d81cd395b2a799e9;
+
+    var forecastApi = api.openweathermap.org/data/2.5/forecast?q={city name}&appid=ab246c1d8eb84670d81cd395b2a799e9;
+
+    var uvApi = api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=ab246c1d8eb84670d81cd395b2a799e9;
+});
+
